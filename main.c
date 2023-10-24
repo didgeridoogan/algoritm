48df38b133923b976635b017877c1fb5503d7d6a
Dilara-E <dogan.dilara@elev.ga.ntig.se>
2023-10-24 14:48:16 +0200

    randomisering och movement

--- main.c
+++ main.c
@@ -1,71 +1,108 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "raylib.h"
+#include <time.h>
 
-typedef struct map_t
-{
-    int tile_size;
-    int tiles[10][10];
-} map_t;
+#define MAP_SIZE 20
+#define PLAYER '@'
+#define EMPTY '.'
+#define WALL '#'
 
-typedef enum tiles_e
-{
-    TILE_FLOOR,
-    TILE_WALL,
-    TILE_WATER
-} tiles_e;
+char map[MAP_SIZE][MAP_SIZE];
 
-void DrawMap(map_t map, int x, int y)
+void initializeMap()
 {
-    for (int y = 0; y < 10; y++)
+    for (int y = 0; y < MAP_SIZE; y++)
     {
-        for (int x = 0; x < 10; x++)
+        for (int x = 0; x < MAP_SIZE; x++)
         {
-            switch (map.tiles[y][x])
+            if (x == 0 || x == MAP_SIZE - 1 || y == 0 || y == MAP_SIZE - 1)
             {
-            case TILE_FLOOR:
-            {
-                DrawRectangle(x * map.tile_size, y * map.tile_size, map.tile_size, map.tile_size, GREEN);
+                map[y][x] = WALL;
             }
-            break;
-            case TILE_WALL:
+            else
             {
-                DrawRectangle(x * map.tile_size, y * map.tile_size, map.tile_size, map.tile_size, ORANGE);
-            }
-            break;
-            case TILE_WATER:
-            {
-                DrawRectangle(x * map.tile_size, y * map.tile_size, map.tile_size, map.tile_size, BLUE);
-            }
+                if (rand() % 5 == 0)
+                {
+                    map[y][x] = WALL; // Randomly placera väggar
+                }
+                else
+                {
+                    map[y][x] = EMPTY;
+                }
             }
         }
     }
 }
 
+void printMap()
+{
+    for (int y = 0; y < MAP_SIZE; y++)
+    {
+        for (int x = 0; x < MAP_SIZE; x++)
+        {
+            printf("%c ", map[y][x]);
+        }
+        printf("\n");
+    }
+}
+
 int main()
 {
-    map_t main_map = (map_t){
-        .tile_size = 16,
-        .tiles = {
-            {1, 1, 1, 2, 1, 1, 1, 1, 1, 1},
-            {1, 0, 0, 2, 0, 0, 0, 0, 0, 1},
-            {1, 0, 0, 2, 0, 0, 0, 0, 0, 1},
-            {1, 0, 0, 2, 0, 0, 0, 0, 0, 1},
-            {1, 0, 1, 2, 2, 2, 0, 1, 1, 1},
-            {1, 0, 1, 1, 0, 2, 0, 0, 0, 1},
-            {1, 0, 0, 0, 0, 2, 0, 0, 0, 1},
-            {1, 0, 0, 0, 0, 2, 0, 0, 0, 1},
-            {1, 0, 0, 0, 0, 2, 0, 0, 0, 1},
-            {1, 1, 1, 1, 1, 2, 1, 1, 1, 1},
-        }};
+    srand(time(NULL));
+    initializeMap();
+    int playerX = MAP_SIZE / 2;
+    int playerY = MAP_SIZE / 2;
 
-    InitWindow(165, 165, "Tilemap");
+    map[playerY][playerX] = PLAYER;
 
-    while (!WindowShouldClose())
+    while (1)
     {
-        BeginDrawing();
-        ClearBackground(BLACK);
-        DrawMap(main_map, 0, 0);
-        EndDrawing();
+        system("clear");
+        printMap();
+
+        char move;
+        printf("Enter a move (w/a/s/d, q to quit): ");
+        scanf(" %c", &move);
+        if (move == 'q')
+        {
+            break; // Stäng spelet om "q" är nedtryckt
+        }
+
+        map[playerY][playerX] = EMPTY;
+
+        switch (move)
+        {
+        case 'w':
+            if (map[playerY - 1][playerX] != WALL)
+            {
+                playerY--;
+            }
+            break;
+
+        case 'a':
+            if (map[playerY][playerX - 1] != WALL)
+            {
+                playerX--;
+            }
+            break;
+
+        case 's':
+            if (map[playerY + 1][playerX] != WALL)
+            {
+                playerY++;
+            }
+            break;
+            
+        case 'd':
+            if (map[playerY][playerX + 1] != WALL)
+            {
+                playerX++;
+            }
+            break;
+        }
+
+        map[playerY][playerX] = PLAYER;
     }
+
+    return 0;
 }
